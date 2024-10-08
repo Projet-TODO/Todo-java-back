38df1264ade7b9fc743e71b389a50189407fb27f
AlexisP <alexis.pouillieute@gmail.com>
2024-10-08 16:24:53 +0200

    changed init to be in Postgre

--- back/initdb/Copy bdd.sql
+++ back/initdb/Copy bdd.sql
@@ -1,51 +1,40 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+-- Script PostgreSQL
 
+-- ------------------------------------------------------------
+-- Table: User
+-- ------------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: User
-#------------------------------------------------------------
+CREATE TABLE "User" (
+                        id_user         SERIAL PRIMARY KEY,
+                        last_name_user  VARCHAR(50) NOT NULL,
+                        first_name_user VARCHAR(50) NOT NULL,
+                        email_user      VARCHAR(50) NOT NULL,
+                        password_user   VARCHAR(50) NOT NULL
+);
 
-CREATE TABLE User(
-        id_user         Int  Auto_increment  NOT NULL ,
-        last_name_user  Varchar (50) NOT NULL ,
-        first_name_user Varchar (50) NOT NULL ,
-        email_user      Varchar (50) NOT NULL ,
-        password_user   Varchar (50) NOT NULL
-	,CONSTRAINT User_PK PRIMARY KEY (id_user)
-)ENGINE=InnoDB;
+-- ------------------------------------------------------------
+-- Table: Project
+-- ------------------------------------------------------------
 
+CREATE TABLE Project (
+                         id_project   SERIAL PRIMARY KEY,
+                         name_project VARCHAR(50) NOT NULL,
+                         date_project DATE NOT NULL,
+                         id_user      INT NOT NULL,
+                         CONSTRAINT Project_User_FK FOREIGN KEY (id_user) REFERENCES "User"(id_user) ON DELETE CASCADE
+);
 
-#------------------------------------------------------------
-# Table: Project
-#------------------------------------------------------------
+-- ------------------------------------------------------------
+-- Table: Task
+-- ------------------------------------------------------------
 
-CREATE TABLE Project(
-        id_project   Int  Auto_increment  NOT NULL ,
-        name_project Varchar (50) NOT NULL ,
-        date_project Date NOT NULL ,
-        id_user      Int NOT NULL
-	,CONSTRAINT Project_PK PRIMARY KEY (id_project)
-
-	,CONSTRAINT Project_User_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Task
-#------------------------------------------------------------
-
-CREATE TABLE Task(
-        id_task          Int  Auto_increment  NOT NULL ,
-        title_task       Varchar (100) NOT NULL ,
-        description_task Varchar (1000) NOT NULL ,
-        priority_task    Int NOT NULL ,
-        deadline_task    Date NOT NULL ,
-        achieved_task    Bool NOT NULL ,
-        id_project       Int NOT NULL
-	,CONSTRAINT Task_PK PRIMARY KEY (id_task)
-
-	,CONSTRAINT Task_Project_FK FOREIGN KEY (id_project) REFERENCES Project(id_project)
-)ENGINE=InnoDB;
-
+CREATE TABLE Task (
+                      id_task          SERIAL PRIMARY KEY,
+                      title_task       VARCHAR(100) NOT NULL,
+                      description_task VARCHAR(1000) NOT NULL,
+                      priority_task    INT NOT NULL,
+                      deadline_task    DATE NOT NULL,
+                      achieved_task    BOOLEAN NOT NULL,
+                      id_project       INT NOT NULL,
+                      CONSTRAINT Task_Project_FK FOREIGN KEY (id_project) REFERENCES Project(id_project) ON DELETE CASCADE
+);
